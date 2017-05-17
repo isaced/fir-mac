1d7c855335f5b36939bd3cad6016ad7b17e5d427
isaced <isaced@163.com>
2017-05-17 22:45:10 +0800

    applicationShouldTerminateAfterLastWindowClosed

--- fir-mac/AppDelegate.swift
+++ fir-mac/AppDelegate.swift
@@ -27,6 +27,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 
     func alertForAPIToken() {
         let msg = NSAlert()
